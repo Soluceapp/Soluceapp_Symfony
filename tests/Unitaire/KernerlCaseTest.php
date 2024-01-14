63124fcabdd95b38a81f79d610540c2549787e1d
Soluceapp <thomas.vervisch@soluceapp.com>
2024-01-14 12:28:42 +0100

    Rect test

--- tests/Unitaire/KernerlCaseTest.php
+++ tests/Unitaire/KernerlCaseTest.php
@@ -2,16 +2,31 @@
 
 namespace App\Tests\Unitaire;
 
+use App\Entity\Scenario;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class KernerlCaseTest extends KernelTestCase
 {
-    public function testSomething(): void
+    public function testEnvironnement(): void
     {
         $kernel = self::bootKernel();
 
-        $this->assertSame('test', $kernel->getEnvironment());
-        // $routerService = static::getContainer()->get('router');
-        // $myCustomService = static::getContainer()->get(CustomService::class);
+        $this->assertSame('test', $kernel->getEnvironment());   
     }
+
+    public function testScenario(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $scenario = new Scenario();
+        $scenario->setNameScenario('Test scenario')
+                ->setNbscenario(1);
+
+        $errors = $container->get('validator')->validate($scenario);
+
+        $this->assertCount(0, $errors);
+    }
+
+
+
 }
